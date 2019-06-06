33cbb517b7f81a390d40d58ea1ff278d3ae0ed1e
E.C <35582713+bwcont@users.noreply.github.com>
2019-06-06 15:35:33 -0500

    Update tidyverse.R

--- tidyverse.R
+++ tidyverse.R
@@ -1,9 +1,25 @@
 ##Installation of packages
 install.packages("gapminder")
 install.packages("dplyr")
- 
+
 ##Load packages
 library(gapminder)
 library(dplyr)
+
+##See available data
+gapminder
+
+##filter verb
+gapminder %>% #%>% is a pipe which is saying to feed the preceding element into the following step
+  filter(year == 2007)
+#the pipe is necessary for filter to be used
+
+gapminder %>% 
+  filter(country == "United States", year == 2007)
+
+
+
+
+
 
 
