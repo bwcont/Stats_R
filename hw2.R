b9a2c954c266b24dd7294f504d2e75a82ca098f2
E.C <35582713+bwcont@users.noreply.github.com>
2019-09-23 14:24:26 -0500

    Create hw2.R

---
+++ hw2.R
@@ -0,0 +1,3 @@
+possibilities <- c("10blue : 0white", "9blue :1white", "8blue :2white", "7blue :3white", "6blue :4white", 
+                   "5blue :5white", "4blue :6white", "3blue : 7white", "2blue :8white", "1blue :9w hite", 
+                   "0blue :10white")
