837fea43db2b78ae41d8b612ac302665af0d28e4
E.C <35582713+bwcont@users.noreply.github.com>
2019-09-23 14:32:05 -0500

    Update hw2.R

--- hw2.R
+++ hw2.R
@@ -1,3 +1,11 @@
 possibilities <- c("10blue : 0white", "9blue :1white", "8blue :2white", "7blue :3white", "6blue :4white", 
                    "5blue :5white", "4blue :6white", "3blue : 7white", "2blue :8white", "1blue :9w hite", 
                    "0blue :10white")
+
+m <- 20 #white tokens
+n <- 50 #blue tokens
+k <- 10 #number of tokens drawn
+
+
+
+phyper(1:10, m, n,k)
