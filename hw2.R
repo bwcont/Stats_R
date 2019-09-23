36b8ff5ed8d5934f1f48e8a13c76b7cc8bce7b9b
E.C <35582713+bwcont@users.noreply.github.com>
2019-09-23 15:08:51 -0500

    Update hw2.R

--- hw2.R
+++ hw2.R
@@ -1,11 +1,10 @@
-possibilities <- c("10blue : 0white", "9blue :1white", "8blue :2white", "7blue :3white", "6blue :4white", 
+combinations <- c("10blue : 0white", "9blue :1white", "8blue :2white", "7blue :3white", "6blue :4white", 
                    "5blue :5white", "4blue :6white", "3blue : 7white", "2blue :8white", "1blue :9w hite", 
                    "0blue :10white")
+probability <-  print(dhyper(0:10, m = 20, n = 50, k = 10))
 
-m <- 20 #white tokens
-n <- 50 #blue tokens
-k <- 10 #number of tokens drawn
+combinations <- as.data.frame(combinations) 
+probability <- as.data.frame(probability)
 
-
-
-phyper(1:10, m, n,k)
+list1 <- cbind(combinations, probability)
+print(list1)
