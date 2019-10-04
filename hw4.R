eeb80ed5c8b0a3012c84fa2cd8cc55e66a29f388
E.C <35582713+bwcont@users.noreply.github.com>
2019-10-04 12:58:13 -0500

    Create hw4.R

---
+++ hw4.R
@@ -0,0 +1,19 @@
+# Make a plot that demonstrates that a Poisson 
+# distribution is a good approximation of a binomial 
+# distribution when n >> 0 and p << 1. To do this, 
+# choose an appropriate set of n and p values, like slide 
+# 51 of Ppt180919. (But note that both distributions in 
+# this assignment are discrete distributions.) 
+# Make a single file that contains: all the 'code' you used for 
+# this (it is fine to copy all the related from the console); 
+# the plot you made as a pasted picture (5 points); including 
+# a legend + plot title + labeled axes that were inserted into
+# your graphic using R (2 points); and an accompanying text 
+# explanation of why this plot demonstrates that a Poisson 
+# distribution is a good approximation of a binomial distribution
+# when n >> 0 and p << 1. Make the explanation understandable to 
+# people who only see your plot and explanation - assume that those
+# people do not see your R scripts (3 points).
+
+
+
